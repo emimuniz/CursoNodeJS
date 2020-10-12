e56a7477cb6cb59acdac2affd2bc12b829529d9c
Emillyn Muniz <emillyn_muniz@hotmail.com>
2020-10-12 20:24:57 -0300

    Add files via upload

---
+++ CursoNodeAlura/Aula02/server.js
@@ -0,0 +1,30 @@
+const http = require('http');
+
+const servidor = http.createServer((req, resp) => {
+  let html = '';
+  if (req.url === '/') {
+    html = `
+            <html>
+                <head>
+                    <meta charset="utf-8">
+                </head>
+                <body>
+                    <h1> Olá Mundo! </h1>
+                </body>
+            </html>
+        `;
+  } else if (req.url === '/livros') {
+    html = `
+        <html>
+            <head>
+                <meta charset="utf-8">
+            </head>
+            <body>
+                <h1>Listagem de Livros! </h1>
+            </body>
+        </html>
+    `;
+  }
+  resp.end(html);
+});
+servidor.listen(3000);
